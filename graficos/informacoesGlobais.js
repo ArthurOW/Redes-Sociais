3356c2246d09cfccf93f50af62cb7dd82392941e
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-09-27 11:53:15 -0300

    paragrafo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,6 +2,9 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de Pessoas, e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma Rede Social e passam em média ${dados.tempo_medio} horas conectadas.`
     console.log(dados);
 }
 visualizarInformacoesGlobais()
