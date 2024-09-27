b875f1973e0f410a5d1ad49a69815d89b649aec8
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-09-27 12:04:06 -0300

    paragrafo 3.0

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,6 +5,8 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de Pessoas, e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma Rede Social e passam em média ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
     console.log(paragrafo);
 }
 visualizarInformacoesGlobais()
