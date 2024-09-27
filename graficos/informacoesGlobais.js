b71652841ab2e346c7b69fd7e245b4fb999605a7
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-09-27 12:05:44 -0300

    deixei "horas conectadas" maiusculo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,7 +4,7 @@ async function visualizarInformacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de Pessoas, e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma Rede Social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de Pessoas, e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma Rede Social e passam em média ${dados.tempo_medio} Horas Conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
     console.log(paragrafo);
