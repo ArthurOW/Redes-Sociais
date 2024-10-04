76f3436237efcf493ef398efe674affe36e73151
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-10-04 09:09:54 -0300

    pessoas

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,11 +2,11 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de Pessoas, e que aproximadamente <span> ${dados.total_pessoas_conectadas} </span> estão conectadas em alguma Rede Social e passam em média <span> ${dados.tempo_medio} </span> Horas Conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de Pessoas, e que aproximadamente <span> ${pessoasConectadas} Bilhões </span> estão conectadas em alguma Rede Social e passam em média <span> ${dados.tempo_medio} </span> Horas Conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    console.log(paragrafo);
 }
 visualizarInformacoesGlobais()
