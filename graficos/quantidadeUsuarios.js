92da162b9e0b56e4114cebad2e678b6a48fbc87e
ArthurOW <126161571+ArthurOW@users.noreply.github.com>
2024-11-01 08:52:01 -0300

    arrumei?

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -13,7 +13,7 @@ async function quantidadeUsuarios() {
     ]
     const grafico = document.createElement('div')
         grafico.className = 'grafico'
-        document.getElementById('graficos-container').appendChild(graficos)
+        document.getElementById('graficos-container').appendChild(grafico)
         Plotly.newPlot(grafico, data)
         
     console.log(dados)
